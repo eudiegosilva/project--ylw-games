3066640ede3b4764e1c68a9ff83ea744977e36f7
Diego Ramos <diego.coderamos@gmail.com>
2020-09-16 00:25:02 -0300

    update component tests

--- src/components/Heading/stories.tsx
+++ src/components/Heading/stories.tsx
@@ -14,5 +14,5 @@ export default {
 export const Default: Story<HeadingProps> = args => <Heading {...args} />;
 
 Default.args = {
-  children: 'Most Populars'
+  children: 'Heading'
 };
--- src/components/Heading/test.tsx
+++ src/components/Heading/test.tsx
@@ -12,20 +12,20 @@ describe('<Heading />', () => {
     });
   });
 
-  it('should render a white heading when color is passed', () => {
+  it('should render a white heading when color prop is passed', () => {
     renderWithTheme(<Heading color="white">YLW games</Heading>);
     expect(screen.getByRole('heading', { name: /YLW games/i })).toHaveStyle({
       color: '#FFFFFF'
     });
   });
 
-  it('should render heading with a line to the left side', () => {
+  it('should render heading with a line to the left side when lineLeft prop is passed', () => {
     renderWithTheme(<Heading lineLeft>YLW games</Heading>);
     expect(screen.getByRole('heading', { name: /YLW games/i })).toHaveStyle({
       'border-left': '0.7rem solid #FFCB00'
     });
   });
-  it('should render heading with a line at the bottom', () => {
+  it('should render heading with a line at the bottom when lineBottom prop is passed', () => {
     renderWithTheme(<Heading lineBottom>YLW games</Heading>);
     expect(screen.getByRole('heading', { name: /YLW games/i })).toHaveStyleRule(
       'border-bottom',

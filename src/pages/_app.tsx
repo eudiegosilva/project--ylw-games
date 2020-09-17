7496ee4f96b48db545961bc8485d9f9f5624fc29
Diego Ramos <diego.coderamos@gmail.com>
2020-09-17 01:53:43 -0300

    update Button component with tests

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,9 @@
-import { AppProps } from 'next/app'
-import Head from 'next/head'
-import { ThemeProvider } from 'styled-components'
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import { ThemeProvider } from 'styled-components';
 
-import GlobalStyles from 'styles/global'
-import theme from 'styles/theme'
+import GlobalStyles from 'styles/global';
+import theme from 'styles/theme';
 
 function App({ Component, pageProps }: AppProps) {
   return (
@@ -21,7 +21,7 @@ function App({ Component, pageProps }: AppProps) {
       <GlobalStyles />
       <Component {...pageProps} />
     </ThemeProvider>
-  )
+  );
 }
 
-export default App
+export default App;

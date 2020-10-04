e9fdd6b4820c69a798758469eaf370683f60259a
Diego Ramos <diego.coderamos@gmail.com>
2020-10-03 22:36:38 -0300

    update project branding

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,13 +9,13 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
       <Head>
-        <title>React Avançado - Boilerplate</title>
+        <title>yellow | example-store-client</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta
           name="description"
-          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+          content="A YELLOW example for a store client with TypeScript, React, NextJS and Styled Components"
         />
       </Head>
       <GlobalStyles />

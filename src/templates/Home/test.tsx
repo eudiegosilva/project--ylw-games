46ced9aefd2a14a156eafa867b80066042cf971c
Diego Ramos <diego.coderamos@gmail.com>
2020-10-05 03:13:40 -0300

    create Home template with tests

--- src/templates/Home/test.tsx
+++ src/templates/Home/test.tsx
@@ -11,14 +11,14 @@ import Home from '.';
 
 const props = {
   banners: bannersMocks,
-  newGames: gamesMocks,
+  newGames: [gamesMocks[0]],
   mostPopularHighlight: highlightMocks,
-  mostPopularGames: gamesMocks,
+  mostPopularGames: [gamesMocks[0]],
   upcommingHighlight: highlightMocks,
-  upcommingGames: gamesMocks,
-  upcommingMoreGames: gamesMocks,
+  upcommingGames: [gamesMocks[0]],
+  upcommingMoreGames: [gamesMocks[0]],
   freeHighlight: highlightMocks,
-  freeGames: gamesMocks
+  freeGames: [gamesMocks[0]]
 };
 
 describe('<Home />', () => {
@@ -34,24 +34,33 @@ describe('<Home />', () => {
   it('should render menu and footer', () => {
     renderWithTheme(<Home {...props} />);
 
+    // menu
     expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument();
+
+    // footer
     expect(
       screen.getByRole('heading', { name: /contact us/i })
     ).toBeInTheDocument();
-  });
 
-  it('should render correctly heading sections', () => {
-    renderWithTheme(<Home {...props} />);
-
+    // section news
     expect(screen.getByRole('heading', { name: /news/i })).toBeInTheDocument();
+
+    // section most popular
     expect(
       screen.getByRole('heading', { name: /most popular/i })
     ).toBeInTheDocument();
+
+    // section most upcomming
     expect(
       screen.getByRole('heading', { name: /upcomming/i })
     ).toBeInTheDocument();
+
+    // section most free games
     expect(
       screen.getByRole('heading', { name: /free games/i })
     ).toBeInTheDocument();
+
+    // card game (five sections with one card eatch)
+    expect(screen.getAllByText(/cyber game/i)).toHaveLength(5);
   });
 });

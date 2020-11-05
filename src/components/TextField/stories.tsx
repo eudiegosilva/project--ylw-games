1a9f97f4ec14fc60c0801c2eefb5832902500adc
Diego Ramos <diego.coderamos@gmail.com>
2020-11-05 11:01:20 -0300

    update storybook catalog

--- src/components/TextField/stories.tsx
+++ src/components/TextField/stories.tsx
@@ -3,7 +3,7 @@ import TextField, { TextFieldProps } from '.';
 import { Email } from '@styled-icons/material-outlined';
 
 export default {
-  title: 'Design System/TextField',
+  title: 'Form/TextField',
   component: TextField,
   args: {
     label: 'e-mail',

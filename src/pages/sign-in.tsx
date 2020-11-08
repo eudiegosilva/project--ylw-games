65f541ac538ed1c924a56646b97da6ec2f73be9f
Diego Ramos <diego.coderamos@gmail.com>
2020-11-08 12:16:04 -0300

    update SignIn and SignUp pages with Form component

--- src/pages/sign-in.tsx
+++ src/pages/sign-in.tsx
@@ -1,5 +1,10 @@
 import Auth from 'templates/Auth';
+import FormSignIn from 'components/FormSignIn';
 
 export default function SignIn() {
-  return <Auth title="sign in">children</Auth>;
+  return (
+    <Auth title="sign in">
+      <FormSignIn />
+    </Auth>
+  );
 }
--- src/pages/sign-up.tsx
+++ src/pages/sign-up.tsx
@@ -1,5 +1,10 @@
 import Auth from 'templates/Auth';
+import FormSignUp from 'components/FormSignUp';
 
 export default function SignUp() {
-  return <Auth title="sign up">children</Auth>;
+  return (
+    <Auth title="sign up">
+      <FormSignUp />
+    </Auth>
+  );
 }
